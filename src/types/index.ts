95d0a8d3f3a95baa7358d36fd41d4120b73a30e3
Marcel Losso <marcel.losso@gmail.com>
2025-07-28 00:50:04 -0300

    Fix: formatting with biome

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,9 @@
 export interface PasswordValidator {
-  message: string;
-  validation?: (password: string) => boolean;
-  regex?: RegExp;
+	message: string;
+	validation?: (password: string) => boolean;
+	regex?: RegExp;
 }
 
 export interface BetterPasswordOptions {
-  validators: PasswordValidator[];
+	validators: PasswordValidator[];
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,41 @@
 import { APIError } from "better-auth/api";
 import type { PasswordValidator } from "./types";
 
-export const validatePassword = (password: string, validators: PasswordValidator[]) => {
-  const errors: string[] = [];
+export const validatePassword = (
+	password: string,
+	validators: PasswordValidator[],
+) => {
+	const errors: string[] = [];
 
-  for (const validator of validators) {
-   let isValid = true;
-   if (validator.regex) {
-    if (validator.regex instanceof RegExp) {
-      isValid = validator.regex.test(password);
-    } else {
-      throw new APIError("BAD_REQUEST", {
-        message: "Invalid regex",
-      });
-    }
-   }
-   if (validator.validation) {
-    try {
-      isValid = validator.validation(password);
-    } catch (error) {
-      throw new APIError("BAD_REQUEST", {
-        message: "Invalid validation"
-      });
-    }
-   }
+	for (const validator of validators) {
+		let isValid = true;
+		if (validator.regex) {
+			if (validator.regex instanceof RegExp) {
+				isValid = validator.regex.test(password);
+			} else {
+				throw new APIError("BAD_REQUEST", {
+					message: "Invalid regex",
+				});
+			}
+		}
+		if (validator.validation) {
+			try {
+				isValid = validator.validation(password);
+			} catch (error) {
+				throw new APIError("BAD_REQUEST", {
+					message: "Invalid validation",
+				});
+			}
+		}
 
-   if (!isValid) {
-    errors.push(validator.message);
-   }
-  }
+		if (!isValid) {
+			errors.push(validator.message);
+		}
+	}
 
-  if (errors.length > 0) {
-    throw new APIError("BAD_REQUEST", {
-      message: errors.join(", "),
-    });
-  }
+	if (errors.length > 0) {
+		throw new APIError("BAD_REQUEST", {
+			message: errors.join(", "),
+		});
+	}
 };
